498616dc6014333e3ad63f0b02abce1d8809fa06
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2020-01-23 10:14:43 +0100

    Added cards instead of jumbotron

--- application/resources/views/index.blade.php
+++ application/resources/views/index.blade.php
@@ -3,17 +3,18 @@
 @section('title', 'Home')
 
 @section('content')
-    <div class="container">
+<div class="container">
+    <div class="row">
         <div class="col-md-12">
-            <div class="jumbotron">
-                <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <hr class="my-4">
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <p class="lead">
-                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-                </p>
+            <div class="card">
+                <div class="card-header">Welkom</div>
+
+                <div class="card-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse turpis mi, ultricies a diam eget, bibendum tristique est. Praesent finibus ut elit tempor mattis. Vestibulum vel fermentum ipsum. Nulla finibus vulputate bibendum. Pellentesque ac dui vestibulum, volutpat enim in, aliquet nibh. Pellentesque lacinia lacinia neque, a semper est pretium eu. Phasellus a erat ligula. Donec sit amet mattis arcu. Maecenas eget imperdiet nulla, sed posuere augue. Praesent in semper nibh. Etiam aliquet, ex in viverra maximus, tellus quam tempor sem, ac semper ante felis non ipsum. In semper imperdiet neque nec venenatis.</p>
+                    <p>Sed vel nunc iaculis, finibus arcu ac, iaculis quam. Aenean enim quam, eleifend ac felis vel, feugiat ultricies nibh. Curabitur ex eros, auctor vel elit et, pulvinar dignissim felis. Mauris mattis mi justo, vehicula faucibus turpis hendrerit id. Proin dignissim sit amet ligula nec commodo. Vestibulum pretium bibendum scelerisque. Maecenas volutpat, justo id feugiat ultricies, sem ligula euismod lectus, in hendrerit metus lorem non purus. Sed mattis ligula ac nisi pulvinar, tincidunt rhoncus nisi tempor.</p>
+                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
